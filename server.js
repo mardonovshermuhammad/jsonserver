b425f8fee01a21b44aa10feb0f8ed482c29e58fc
mardonovshermuhammad <mardonovshermuhammad@gmail.com>
2022-11-30 10:09:06 +0500

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+setServers.use(middlewares);
+setServers.use(router);
+setServers.use(port);
