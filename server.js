d1962cbf2b0156586ba1f40db1a5799b97e17451
mardonovshermuhammad <mardonovshermuhammad@gmail.com>
2022-11-30 10:21:46 +0500

    add modules 3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
 const jsonServer = require('json-server')
-const setServers = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-setServers.use(middlewares);
-setServers.use(router);
-setServers.use(port);
+server.use(middlewares);
+server.use(router);
+server.listen(port);
